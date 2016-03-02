b969d02085f769efbd313e8ff301cb3dd0bd0cb3
Dmitri Kourennyi <dkour@mykolab.com>
2016-03-02 16:11:56 -0500

    Formatting cleanup

--- src/data/checkContrast.js
+++ src/data/checkContrast.js
@@ -1,11 +1,11 @@
 var darkColor;
 var lightColor;
 
-self.port.on("colors", function(colors) {
+self.port.on("colors", function (colors) {
     darkColor = colors[0];
     lightColor = colors[1];
     allColors = colors[2];
-//     console.log("Contrast: got colors, checking elements");
+    //     console.log("Contrast: got colors, checking elements");
 
     if (allColors == true) {
         // Now replace document colors
@@ -15,9 +15,9 @@ self.port.on("colors", function(colors) {
         if (document.compatMode == "BackCompat") {
             // tables don't inherit color
             var tables = document.getElementsByTagName("table");
-            for (var i=0; i < tables.length; i++) {
-                if (getComputedStyle(tables[i]).color
-                    == getDefaultComputedStyle(tables[i]).color) {
+            for (var i = 0; i < tables.length; i++) {
+                if (getComputedStyle(tables[i]).color ==
+                    getDefaultComputedStyle(tables[i]).color) {
                     //if color has not been set explicitely, then force inherit
                     tables[i].style.color = "inherit";
                 }
@@ -47,25 +47,26 @@ self.port.on("colors", function(colors) {
     }
 });
 
-function checkElementContrast(element)
-{
+function checkElementContrast(element) {
     try {
-        var isFgUndefined = (getComputedStyle(element).color == getDefaultComputedStyle(element).color);
-        var isBgUndefined = (getComputedStyle(element).backgroundColor == getDefaultComputedStyle(element).backgroundColor)
+        var isFgUndefined = (getComputedStyle(element).color ==
+            getDefaultComputedStyle(element).color);
+        var isBgUndefined = (getComputedStyle(element).backgroundColor ==
+            getDefaultComputedStyle(element).backgroundColor)
 
         var hasBgImg = (getComputedStyle(element).backgroundImage != 'none');
-    }
-    catch (e) {
-        console.log("Got exception " + e.message + " when parsing element " + element.tagName + "." + element.className + "#"  + element.id);
+    } catch (e) {
+        console.log("Got exception " + e.message + " when parsing element " +
+            element.tagName + "." + element.className + "#" + element.id);
         return;
     }
 
     isBgUndefined = isBgUndefined && !hasBgImg;
 
-    var fg_color_defined = getComputedStyle(element).color
-                        != getDefaultComputedStyle(element).color;
-    var bg_color_defined = getComputedStyle(element).backgroundColor
-                        != getDefaultComputedStyle(element).backgroundColor;
+    var fg_color_defined = getComputedStyle(element).color !=
+        getDefaultComputedStyle(element).color;
+    var bg_color_defined = getComputedStyle(element).backgroundColor !=
+        getDefaultComputedStyle(element).backgroundColor;
     var bg_img_defined = getComputedStyle(element).backgroundImage != 'none';
 
     if (fg_color_defined && bg_color_defined) {
@@ -96,19 +97,19 @@ function checkElementContrast(element)
 
     //Nothing defined, recurse through children
     var children = element.children
-    for (var i=0; i < children.length; i++) {
+    for (var i = 0; i < children.length; i++) {
         // Don't look at non-renderable elements
         switch (element.children[i].nodeName) {
-            case "HEAD":
-            case "TITLE":
-            case "META":
-            case "SCRIPT":
-            case "IMG":
-            case "STYLE":
-                break;
-            default:
-                //console.log("Contrast: recursing");
-                checkElementContrast(element.children[i]);
+        case "HEAD":
+        case "TITLE":
+        case "META":
+        case "SCRIPT":
+        case "IMG":
+        case "STYLE":
+            break;
+        default:
+            //console.log("Contrast: recursing");
+            checkElementContrast(element.children[i]);
         }
     }
     return;
