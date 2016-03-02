6284987024b0fe984e27fbc8bec3255223f9e7af
Dmitri Kourennyi <dkour@mykolab.com>
2016-03-02 16:08:29 -0500

    Try to match any possible page for processing.
    Use a regex that should select any kind of url for processing. Hopefully
    this ensures the addons runs on all browser internal pages like resource://
    and chrome:// and so on.

--- src/lib/main.js
+++ src/lib/main.js
@@ -18,7 +18,7 @@ require('sdk/simple-prefs').on('allColors', cacheColors);
 
 // Create page mod to fix colors
 pageMod.PageMod({
-  include: ["*", "file://*", "about:*", "resource://*"],
+  include: /.*/,
   contentScriptFile : './checkContrast.js',
   onAttach: function(worker) {
     //console.log("Contrast: added pagemod for: " + worker.tab.title);
