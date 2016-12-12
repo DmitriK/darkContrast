9be938f44fb3a26ff7c106f781737b59c37048d3
Dmitri Kourennyi <dkour@mykolab.com>
2016-12-12 12:14:53 -0500

    Remove leftover console logging

--- src/bg.js
+++ src/bg.js
@@ -2,9 +2,7 @@
 
 function sendToggle(tabs) {
   let id = tabs[0].id;
-  console.log(id);
   browser.tabs.sendMessage(id, {request: 'toggle'}).then(m => {
-    console.log("got response?", m, id);
     if (m.toggle) {
       browser.browserAction.setBadgeText({text: "", tabId: id});
     } else {
