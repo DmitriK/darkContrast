1a366993d5e04c56303e921f27bda0484d6e7d56
Dmitri Kourennyi <dkour@mykolab.com>
2017-03-01 15:17:55 -0500

    Remove unused code in background script

--- src/bg.js
+++ src/bg.js
@@ -1,16 +1,3 @@
-"use strict";
-
-function sendToggle(tabs) {
-  let id = tabs[0].id;
-  browser.tabs.sendMessage(id, {request: 'toggle'}).then(m => {
-    if (m.toggle) {
-      browser.browserAction.setBadgeText({text: "", tabId: id});
-    } else {
-      browser.browserAction.setBadgeText({text: "🞬", tabId: id});
-    }
-  });
-}
-
-browser.browserAction.onClicked.addListener(() => {
-  browser.tabs.query({currentWindow: true, active: true}).then(sendToggle);
-});
+/* Copyright (c) 2016 Dmitri Kourennyi */
+/* See the file COPYING for copying permission. */
+'use strict';
