1a8e0c3e5c35fc6db9aa2ab51c868b3a1bdf55c2
Dmitri Kourennyi <dkour@mykolab.com>
2017-03-01 14:02:49 -0500

    Use stricter ESlint rules

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,26 +1,44 @@
 module.exports = {
-    "env": {
-        "browser": true,
-	"webextensions": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  "env": {
+    "browser": true,
+    "webextensions": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "arrow-parens": "error",
+    "comma-dangle": ["warn", "always-multiline"],
+    "eqeqeq": ["error", "smart"],
+    "func-names": ["warn", "as-needed"],
+    "indent": ["error", 2],
+    "init-declarations": "error",
+    "linebreak-style": ["error", "unix"],
+    "newline-after-var": "warn",
+    "newline-before-return": "warn",
+    "no-else-return": "error",
+    "no-extra-parens": "warn",
+    "no-plusplus": "error",
+    "no-trailing-spaces": "error",
+    "no-undefined": "error",
+    "no-use-before-define": "warn",
+    "no-var": "error",
+    "object-property-newline": ["warn", {
+      "allowMultiplePropertiesPerLine": true
+    }],
+    "object-shorthand": "error",
+    "one-var": ["error", "never"],
+    "prefer-arrow-callback": "warn",
+    "prefer-const": "warn",
+    "prefer-destructuring": "warn",
+    "quote-props": ["warn", "consistent-as-needed"],
+    "quotes": ["warn", "single"],
+    "radix": "error",
+    "semi": ["error", "always"],
+    "space-before-function-paren": ["error", {
+      "anonymous": "always",
+      "named": "never",
+      "asyncArrow": "always"
+    }],
+    "spaced-comment": "error",
+  }
 };
