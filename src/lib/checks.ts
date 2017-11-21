898451ad2c63e19a77861956457888a5f820b9f0
Dmitri Kourennyi <dkour@mykolab.com>
2017-11-21 13:43:41 -0500

    Don't send messages to SVG and EMBED elems; there's no contentWindow prop.

--- src/lib/checks.ts
+++ src/lib/checks.ts
@@ -5,7 +5,7 @@ declare function getDefaultComputedStyle(elt: Element, pseudoElt?: string): CSSS
 export const isInputNode = (node: HTMLElement) => (['INPUT', 'TEXTAREA', 'SELECT', 'BUTTON']).indexOf(node.nodeName) > -1;
 export const isInVisibleNode = (node: Node) => (['HEAD', 'TITLE', 'META', 'SCRIPT', 'IMG', 'STYLE', 'BR', 'LINK',
                                                  '#text', 'FRAMESET',]).indexOf(node.nodeName) > -1;
-export const isSubDocNode = (node: Node) => (['IFRAME', 'SVG', 'OBJECT', 'EMBED', 'FRAME']).indexOf(node.nodeName) > -1;
+export const isSubDocNode = (node: Node) => (['IFRAME', 'FRAME', 'OBJECT']).indexOf(node.nodeName) > -1;
 
 export const isFgDefined = (e: HTMLElement): boolean => getComputedStyle(e).color !== getDefaultComputedStyle(e).color;
 export const isBgDefined = (e: HTMLElement): boolean => getComputedStyle(e).backgroundColor !== getDefaultComputedStyle(e).backgroundColor;
