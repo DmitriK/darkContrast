6eb078e045983cf1f5d5a1d9f71cca06b541de5b
Dmitri Kourennyi <dkour@mykolab.com>
2017-12-14 14:44:53 -0500

    Stop listening to frame fix events once one has already been received

--- src/frameListener.ts
+++ src/frameListener.ts
@@ -1,9 +1,13 @@
+const requestStdColors = (e: MessageEvent) => {
+  if (e.data === '_tcfdt_subdoc_std') {
+    browser.runtime.sendMessage({request: 'stdFg'});
+    (window as any).tcfdtFrameFixed = true;
+  }
+  e.stopPropagation();
+  // No need to listen to more messages if we have fixed ourselves
+  window.removeEventListener('message', requestStdColors, {capture: true});
+};
+
 if (window.self !== window.top) {
-  window.addEventListener('message', (e) => {
-    if (e.data === '_tcfdt_subdoc_std') {
-      browser.runtime.sendMessage({request: 'stdFg'});
-      (window as any).tcfdtFrameFixed = true;
-    }
-    e.stopPropagation();
-  }, true);
+  window.addEventListener('message', requestStdColors, {capture: true});
 }
