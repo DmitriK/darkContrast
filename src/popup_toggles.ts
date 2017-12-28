a9b6d1a2b6b8a7c27f0988650cf691d17ff0b6db
Dmitri Kourennyi <dkour@mykolab.com>
2017-12-28 15:23:18 -0500

    Add buttons for adding current site to override lists (GH-38)

--- src/popup_toggles.ts
+++ src/popup_toggles.ts
@@ -1,7 +1,7 @@
 /* Copyright (c) 2017 Dmitri Kourennyi */
 /* See the file COPYING for copying permission. */
 
-const { runtime } = browser;
+const { runtime, storage, tabs } = browser;
 
 window.addEventListener('load', () => {
   (document.getElementById('tog_main') as HTMLButtonElement).addEventListener('click', () => {
@@ -11,12 +11,86 @@ window.addEventListener('load', () => {
   });
 
   (document.getElementById('tog_std') as HTMLButtonElement).addEventListener('click', () => {
-     browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+     tabs.query({currentWindow: true, active: true}).then((tabs) => {
        runtime.sendMessage('', {request: 'std', allFrames: true, tabId: tabs[0].id});
      });
   });
 
   (document.getElementById('open_opts') as HTMLButtonElement).addEventListener('click', () => {
-    browser.runtime.openOptionsPage();
+    runtime.openOptionsPage();
+  });
+
+  storage.local.get('tcfdt-wlist').then((opts) => {
+    let wList: boolean = opts['tcfdt-wlist'] as boolean;
+
+    let el = document.getElementById('listKind') as HTMLSpanElement;
+
+    el.textContent = wList ? 'white' : 'black';
+  });
+
+  tabs.query({currentWindow: true, active: true}).then((tabs) => {
+    let url: string = tabs[0].url!;
+
+    let locProbe = document.createElement('a');
+    locProbe.href = url;
+
+    let host = locProbe.hostname;
+    let path = `${host}${locProbe.pathname}`;
+
+    document.querySelectorAll('.hostEx').forEach((el) => {
+      el.textContent = host;
+    });
+    document.querySelectorAll('.pathEx').forEach((el) => {
+      el.textContent = path;
+    });
+    document.querySelectorAll('.fullEx').forEach((el) => {
+      el.textContent = url;
+    });
+
+    for (let id of [
+         'add_hst_ovr', 'add_pth_ovr', 'add_url_ovr',
+         'add_hst_std', 'add_pth_std', 'add_url_std',
+         ]) {
+      let btn = (document.getElementById(id) as HTMLButtonElement);
+      btn.addEventListener('click', (e: Event) => {
+        let idStr: string = (e.target as HTMLButtonElement).id;
+        let [, part, list] = idStr.split('_');
+
+        let entry: string;
+
+        if (part === 'hst') {
+          entry = host;
+        } else if (part === 'pth') {
+          entry = path;
+        } else if (part === 'url') {
+          entry = url;
+        } else {
+          return;
+        }
+
+        let tObj: 'tcfdt-list-standard' | 'tcfdt-list-disabled';
+
+        if (list === 'std') {
+          tObj = 'tcfdt-list-standard';
+        } else if (list === 'ovr') {
+          tObj = 'tcfdt-list-disabled';
+        } else {
+          return;
+        }
+
+        storage.local.get(tObj).then((opts) => {
+          let list: string[] = [];
+          if (tObj in opts) {
+            list = opts[tObj] as string[];
+          }
+          if (list.indexOf(entry) === -1) {
+            list.push(entry);
+          }
+          let newStore: {[key: string] : string[]} = {};
+          newStore[tObj] = list;
+          storage.local.set(newStore);
+        });
+      });
+    }
   });
 });
