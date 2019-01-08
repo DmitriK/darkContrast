a1c9a1369bfbcd4d45e66e815cec10845321642e
Dmitri Kourennyi <dkour@mykolab.com>
2019-01-08 16:55:08 -0500

    Add comments as invisible node

--- src/lib/checks.ts
+++ src/lib/checks.ts
@@ -28,6 +28,6 @@ export const INPUT_PERMS: Array<InputPermutation> = [
 
 export const isInputNode = (node: HTMLElement) => (INPUT_NODES).indexOf(node.nodeName) > -1;
 export const isInVisibleNode = (node: Node) => (['#text', 'IMG', 'HEAD', 'TITLE', 'META', 'SCRIPT', 'STYLE', 'BR',
-    'LINK', 'FRAMESET',]).indexOf(node.nodeName) > -1;
+    'LINK', 'FRAMESET', '#comment']).indexOf(node.nodeName) > -1;
 export const isSubDocNode = (node: Node) => (['IFRAME', 'FRAME', 'OBJECT']).indexOf(node.nodeName) > -1;
 export const isDontRecurseNode = (node: Node) => (['svg']).indexOf(node.nodeName) > -1;
